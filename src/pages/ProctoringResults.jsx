5c3b9a2459527b1cf440b36dc81a35b77370f975
galitsynaalina <galitsyna@mc.asu.ru>
2025-05-15 17:38:05 +0700

    Сделала хедер и добавила поиск

--- src/pages/ProctoringResults.jsx
+++ src/pages/ProctoringResults.jsx
@@ -9,10 +9,20 @@ const ProctoringResults = () => {
         <div>
         <header className="header-style">
             <div className="div-conteiner-header">
+            <div className="menu-area">  
             <button className="button-menu"></button>
+            </div>
+            <div className="user-exit">
             <span className="username">Пользователь</span>
+            <button className="button-exit"></button>
+            </div>
             </div>
         </header>
+        </div>
+        <div className="div-search">
+            <input name="search_for_student" type="text" placeholder="Поиск по студенту"/>
+            <input/>
+            <input/>
         </div>
         <div>
         <footer className="footer-style"/>
