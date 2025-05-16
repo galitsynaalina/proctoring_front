59e3a189985697c149c1668180ff76e8787e5832
galitsynaalina <galitsyna@mc.asu.ru>
2025-05-16 16:07:05 +0700

    Добавила таблицу результатов

--- src/pages/ProctoringResults.jsx
+++ src/pages/ProctoringResults.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "../index.css";
-// import "@blueprintjs/core/lib/css/blueprint.css";
-// import { Button } from "@blueprintjs/core";
+import "../css/proctoring_results.css";
+import "@radix-ui/themes/styles.css";
+import { Table, Theme } from "@radix-ui/themes";
 
 const ProctoringResults = () => {
     return (
@@ -19,13 +19,56 @@ const ProctoringResults = () => {
             </div>
         </header>
         </div>
+        <div className="div-title">
+            <h3 className="proctoring-results-title">Результаты прокторинга</h3>
+        </div>
         <div className="div-search">
-            <input name="search_for_student" type="text" placeholder="Поиск по студенту"/>
-            <input/>
-            <input/>
+            <input className="search_by_student" name="search_by_student" type="text" placeholder="Поиск по студенту"/>
+            <input className="search_by_subject" name="search_by_subject" type="text" placeholder="Поиск по предмету"/>
+            <input className="search_by_type" name="search_by_type" type="text" placeholder="Поиск по типу"/>
+        </div>
+        <div className="div-table">
+         <Theme panelBackground="solid" radius="none"> 
+         <Table.Root variant="surface">
+	      <Table.Header>
+		    <Table.Row>
+			<Table.ColumnHeaderCell>Студент</Table.ColumnHeaderCell>
+			<Table.ColumnHeaderCell>Предмет</Table.ColumnHeaderCell>
+			<Table.ColumnHeaderCell>Тип прокторинга</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Отсутствие студента</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Лишний человек</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Другой человек</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Взгляд в сторону</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Разговор</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Подсказки</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+		     </Table.Row>
+	     </Table.Header>
+
+	<Table.Body>
+		<Table.Row>
+			<Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+			<Table.Cell>danilo@example.com</Table.Cell>
+			<Table.Cell>Developer</Table.Cell>
+		</Table.Row>
+
+		<Table.Row>
+			<Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+			<Table.Cell>zahra@example.com</Table.Cell>
+			<Table.Cell>Admin</Table.Cell>
+		</Table.Row>
+
+		<Table.Row>
+			<Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+			<Table.Cell>jasper@example.com</Table.Cell>
+			<Table.Cell>Developer</Table.Cell>
+		</Table.Row>
+	        </Table.Body>
+           </Table.Root>
+          </Theme>
         </div>
         <div>
-        <footer className="footer-style"/>
+            <footer className="footer-style"/>
         </div>
     </div>
     );
