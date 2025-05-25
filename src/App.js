aafe848b15b8e275fdf7f601fe2fc2cec32e6f45
galitsynaalina <galitsyna.alina@gmail.com>
2025-05-26 00:50:57 +0700

    Доделала меню

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/proctoring_results" element={<ProctoringResults />} />
+      <Route path="/proctoring-results" element={<ProctoringResults />} />
     </Routes>
     </BrowserRouter>
   );
--- src/pages/ProctoringResults.jsx
+++ src/pages/ProctoringResults.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../css/proctoring_results.css";
 import "@radix-ui/themes/styles.css";
 import '@coreui/coreui/dist/css/coreui.min.css'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { Avatar } from 'primereact/avatar';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+import { StyleClass } from 'primereact/styleclass';
+import { Ripple } from 'primereact/ripple';
 import { Table, Theme } from "@radix-ui/themes";
-import {CBadge,
+import {
+  CBadge,
   CSidebar,
   CSidebarBrand,
   CSidebarHeader,
@@ -11,115 +20,118 @@ import {CBadge,
   CSidebarToggler,
   CNavGroup,
   CNavItem,
-  CNavTitle,} from '@coreui/react'
+  CNavTitle,
+} from '@coreui/react'
 
 const ProctoringResults = () => {
-    return (
+  const [visible, setVisible] = useState(false);
+  const btnRef1 = useRef(null);
+  const btnRef2 = useRef(null);
+  const btnRef3 = useRef(null);
+  const btnRef4 = useRef(null);
+  return (
     <div>
-        <div>
+      <div>
         <header className="header-style">
-            <div className="div-conteiner-header">
-            <div className="menu-area">  
-            <button className="button-menu"></button>
+          <div className="div-conteiner-header">
+            <div className="menu-area">
+              {/* <button className="button-menu"></button> */}
+              <Button className="button-menu" onClick={() => setVisible(true)} />
             </div>
             <div className="user-exit">
-            <span className="username">Пользователь</span>
-            <button className="button-exit" name="button-exit"></button>
+              <span className="username">Пользователь</span>
+              <button className="button-exit" name="button-exit"></button>
             </div>
-            </div>
+          </div>
         </header>
-        
-  
-    <CSidebar className="border-end" unfoldable>
-      <CSidebarHeader className="border-bottom">
-        <CSidebarBrand>CUI</CSidebarBrand>
-      </CSidebarHeader>
-      <CSidebarNav>
-        <CNavTitle>Nav Title</CNavTitle>
-        <CNavItem href="#">
-        </CNavItem>
-        <CNavItem href="#">
-          <CBadge color="primary ms-auto">NEW</CBadge>
-        </CNavItem>
-        <CNavGroup
-          toggler={
-            <>
-            </>
-          }
-        >
-          <CNavItem href="#">
-            <span className="nav-icon">
-              <span className="nav-icon-bullet"></span>
-            </span>{' '}
-            Nav dropdown item
-          </CNavItem>
-          <CNavItem href="#">
-            <span className="nav-icon">
-              <span className="nav-icon-bullet"></span>
-            </span>{' '}
-            Nav dropdown item
-          </CNavItem>
-        </CNavGroup>
-        <CNavItem href="https://coreui.io">
-        </CNavItem>
-        <CNavItem href="https://coreui.io/pro/">
-        </CNavItem>
-      </CSidebarNav>
-    </CSidebar>
-  
+        <Sidebar
+          visible={visible}
+          onHide={() => setVisible(false)}
+          content={({ closeIconRef, hide }) => (
+            <div className="min-h-screen flex relative lg:static surface-ground">
+              <div id="app-sidebar-2" className="surface-section h-screen block flex-shrink-0 absolute lg:static left-0 top-0 z-1 border-right-1 surface-border select-none">
+                <div>
+                  <header className="header-style">
+                    <Button type="button" ref={closeIconRef} onClick={(e) => hide(e)} className="button-menu"></Button>
+                  </header>
+                  <div>
+                    <div className="menu-item">
+                      <span className="menu-item-text">Результаты</span>
+                    </div>
+                    <div className="menu-item">
+                      <span className="menu-item-text">Типы прокторинга</span>
+                    </div>
+                    <div className="menu-item">
+                      <span className="menu-item-text">Прокторинги</span>
 
-        </div>
-        <div className="div-title">
-            <h3 className="proctoring-results-title">Результаты прокторинга</h3>
-        </div>
-        <div className="div-search">
-            <input className="search_by_student" name="search_by_student" type="text" placeholder="Поиск по студенту"/>
-            <input className="search_by_subject" name="search_by_subject" type="text" placeholder="Поиск по предмету"/>
-            <input className="search_by_type" name="search_by_type" type="text" placeholder="Поиск по типу"/>
-        </div>
-        <div className="div-table">
-         <Theme panelBackground="solid" radius="none"> 
-         <Table.Root variant="surface">
-	      <Table.Header>
-		    <Table.Row>
-			<Table.ColumnHeaderCell>Студент</Table.ColumnHeaderCell>
-			<Table.ColumnHeaderCell>Предмет</Table.ColumnHeaderCell>
-			<Table.ColumnHeaderCell>Тип прокторинга</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Отсутствие студента</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Лишний человек</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Другой человек</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Взгляд в сторону</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Разговор</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Подсказки</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-		     </Table.Row>
-	     </Table.Header>
+                    </div>
+                    <div className="menu-item">
+                      <span className="menu-item-text">Роли</span>
+                    </div>
+                    <div className="menu-item">
+                      <span className="menu-item-text">Пользователи</span>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <footer className="footer-style" />
+                </div>
+              </div>
+            </div>
+          )}
+        ></Sidebar>
+      </div>
+      <div className="div-title">
+        <h3 className="proctoring-results-title">Результаты прокторинга</h3>
+      </div>
+      <div className="div-search">
+        <input className="search_by_student" name="search_by_student" type="text" placeholder="Поиск по студенту" />
+        <input className="search_by_subject" name="search_by_subject" type="text" placeholder="Поиск по предмету" />
+        <input className="search_by_type" name="search_by_type" type="text" placeholder="Поиск по типу" />
+      </div>
+      <div className="div-table">
+        <Theme panelBackground="solid" radius="none" style={{minHeight: "100px"}}>
+          <Table.Root variant="surface">
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Студент</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Предмет</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Тип прокторинга</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Отсутствие студента</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Лишний человек</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Другой человек</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Взгляд в сторону</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Разговор</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Подсказки</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+              </Table.Row>
+            </Table.Header>
 
-	<Table.Body>
-		<Table.Row>
-			<Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-			<Table.Cell>danilo@example.com</Table.Cell>
-			<Table.Cell>Developer</Table.Cell>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>1</Table.Cell>
-            <Table.Cell>123434</Table.Cell>
-            <Table.Cell>
-                <button className="button-delete" name="button-delete"></button>
-                <button className="button-edit" name="button-edit"></button>
-            </Table.Cell>
-		</Table.Row>
-	    </Table.Body>
-           </Table.Root>
-          </Theme>
-        </div>
-        <div>
-            <footer className="footer-style"/>
-        </div>
+            <Table.Body>
+              <Table.Row>
+                <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+                <Table.Cell>danilo@example.com</Table.Cell>
+                <Table.Cell>Developer</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+                <Table.Cell>1</Table.Cell>
+                <Table.Cell>123434</Table.Cell>
+                <Table.Cell>
+                  <button className="button-delete" name="button-delete"></button>
+                  <button className="button-edit" name="button-edit"></button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table.Root>
+        </Theme>
+      </div>
+      <div>
+        <footer className="footer-style" />
+      </div>
     </div>
-    );
-};  
+  );
+};
 
 export default ProctoringResults;
