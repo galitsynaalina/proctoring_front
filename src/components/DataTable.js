c497f097465139e251ee3c364698194b9590c424
galitsynaalina <galitsyna.alina@gmail.com>
2025-05-27 19:20:10 +0700

    Доделать события

---
+++ src/components/DataTable.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import "../css/proctoring_results.css";
+
+const Table = () => {
+    const results = [
+        {
+            id: 1,
+            student: "Иванов Иван",
+            subject: "Машинное обучение",
+            proctoring_type: "Оснвные параметры",
+            no_student: false,
+            extra_person: false,
+            other_person: false,
+            averting_the_gaze: true,
+            talk: false,
+            hints: true
+        },
+    ];
+
+    const button_delete = (rowData, options) => {
+        return <button className="button-delete" name="button-delete"/>;
+    };
+
+    const button_edit = (rowData, options) => {
+        return <button className="button-edit" name="button-edit"/>;
+    };
+
+    return (
+        <DataTable stripedRows value={results} tableStyle={{ minWidth: '50rem' }}>
+            <Column field="student" header="Студент"></Column>
+            <Column field="subject" header="Предмет"></Column>
+            <Column field="proctoring_type" header="Тип прокторинга"></Column>
+            <Column field="no_student" header="Отсутствие студента"></Column>
+            <Column field="extra_person" header="Лишний человек"></Column>
+            <Column field="other_person" header="Другой человек"></Column>
+            <Column field="averting_the_gaze" header="Взгляд в сторону"></Column>
+            <Column field="talk" header="Разговор"></Column>
+            <Column field="hints" header="Подсказки"></Column>
+            <Column field="" header=" " style={{ minWidth: '30px' }} body={button_delete}></Column>
+            <Column field="" header=" " body={button_edit}></Column>
+
+        </DataTable>
+    );
+};
+
+export default Table;
--- src/pages/ProctoringResults.jsx
+++ src/pages/ProctoringResults.jsx
@@ -11,22 +11,24 @@ import { Button } from 'primereact/button';
 import { StyleClass } from 'primereact/styleclass';
 import { Ripple } from 'primereact/ripple';
 // import { Table, Theme } from "@radix-ui/themes";
-import Table from "../components/Table";
+import DataTable from "../components/DataTable";
+import SidebarComponent from "../components/SideBar";
 
 const ProctoringResults = () => {
+  
   const [visible, setVisible] = useState(false);
-  const btnRef1 = useRef(null);
-  const btnRef2 = useRef(null);
-  const btnRef3 = useRef(null);
-  const btnRef4 = useRef(null);
+
+  const ToggleSidebar = () => {
+    !visible ? setVisible(true) : setVisible(false);
+  }
+
   return (
     <div>
       <div>
         <header className="header-style">
           <div className="div-conteiner-header">
             <div className="menu-area">
-              {/* <button className="button-menu"></button> */}
-              <Button className="button-menu" onClick={() => setVisible(true)} />
+              <Button className="button-menu" onClick={ToggleSidebar} />
             </div>
             <div className="user-exit">
               <span className="username">Пользователь</span>
@@ -34,41 +36,7 @@ const ProctoringResults = () => {
             </div>
           </div>
         </header>
-        <Sidebar
-          visible={visible}
-          onHide={() => setVisible(false)}
-          content={({ closeIconRef, hide }) => (
-            <div className="min-h-screen flex relative lg:static surface-ground">
-              <div id="app-sidebar-2" className="surface-section h-screen block flex-shrink-0 absolute lg:static left-0 top-0 z-1 border-right-1 surface-border select-none">
-                <div>
-                  <header className="header-style">
-                    <Button type="button" ref={closeIconRef} onClick={(e) => hide(e)} className="button-menu"></Button>
-                  </header>
-                  <div>
-                    <div className="menu-item">
-                      <span className="menu-item-text">Результаты</span>
-                    </div>
-                    <div className="menu-item">
-                      <span className="menu-item-text">Типы прокторинга</span>
-                    </div>
-                    <div className="menu-item">
-                      <span className="menu-item-text">Прокторинги</span>
-                    </div>
-                    <div className="menu-item">
-                      <span className="menu-item-text">Роли</span>
-                    </div>
-                    <div className="menu-item">
-                      <span className="menu-item-text">Пользователи</span>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <footer className="footer-style" />
-                </div>
-              </div>
-            </div>
-          )}
-        ></Sidebar>
+        <SidebarComponent></SidebarComponent>
       </div>
       <div className="div-title">
         <h3 className="proctoring-results-title">Результаты прокторинга</h3>
@@ -79,7 +47,7 @@ const ProctoringResults = () => {
         <input className="search_by_type" name="search_by_type" type="text" placeholder="Поиск по типу" />
       </div>
       <div className="div-table">
-        <Table/>
+        <DataTable />
       </div>
       <div>
         <footer className="footer-style" />
