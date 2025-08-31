acdb2c0097a384b8a81612a1957334a0ef874330
galitsynaalina <galitsyna.alina@gmail.com>
2025-09-01 02:03:03 +0700

    sidebar вынесен в отдельный компонент

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,68 @@
+import { useRef } from "react"; 
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
+interface SidebarMenuProps {
+    visible: boolean;
+    onHide: () => void;
+    username: string | null;
+}
+
+const SidebarMenu = ({ visible, onHide, username }: SidebarMenuProps) => {
+    const closeIconRef = useRef<Button>(null); 
+
+    return (
+        <Sidebar
+            visible={visible}
+            onHide={onHide}
+            content={({ closeIconRef: sidebarCloseIconRef, hide }) => (
+                <div className="min-h-screen flex relative lg:static surface-ground">
+                    <div
+                        id="app-sidebar-2"
+                        className="surface-section h-screen block flex-shrink-0 absolute lg:static left-0 top-0 z-1 border-right-1 surface-border select-none"
+                    >
+                        <div>
+                            <header className="header-style">
+                                <Button
+                                    className="button-menu"
+                                    type="button"
+                                    ref={closeIconRef as React.Ref<Button>}
+                                    onClick={(e) => hide(e)}
+                                />
+                            </header>
+                            <div>
+                                <a href="/proctoring-results" className="menu-item">
+                                    <div className="menu-item-text">Результаты</div>
+                                </a>
+                                <a href="/proctoring-types" className="menu-item">
+                                    <div className="menu-item-text">Типы прокторинга</div>
+                                </a>
+                                <a href="/proctoring" className="menu-item">
+                                    <div className="menu-item-text">Прокторинги</div>
+                                </a>
+                                {username === 'admin' && (
+                                    <>
+                                        <a href="/roles" className="menu-item">
+                                            <div className="menu-item-text">Роли</div>
+                                        </a>
+                                        <a href="/users" className="menu-item">
+                                            <div className="menu-item-text">Пользователи</div>
+                                        </a>
+                                    </>
+                                )}
+                                <a href="/subjects" className="menu-item">
+                                    <div className="menu-item-text">Предметы</div>
+                                </a>
+                            </div>
+                        </div>
+                        <div>
+                            <footer className="footer-style" />
+                        </div>
+                    </div>
+                </div>
+            )}
+        />
+    );
+};
+
+export default SidebarMenu;
--- src/pages/CreateType.tsx
+++ src/pages/CreateType.tsx
@@ -6,9 +6,9 @@ import "../css/footer.css"
 import '@coreui/coreui/dist/css/coreui.min.css'
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
-import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
 import Footer from "../components/Footer";
+import SidebarMenu from "../components/Sidebar";
 
 
 interface FormData {
@@ -79,47 +79,11 @@ const CreateType = () => {
           <div className="div-container-header">
             <div className="menu-area">
               <Button className="button-menu" onClick={() => { setVisible(true) }} />
-              <Sidebar visible={visible}
-                onHide={() => { setVisible(false) }}
-                content={({ closeIconRef, hide }) => (
-                  <div className="min-h-screen flex relative lg:static surface-ground">
-                    <div id="app-sidebar-2" className="surface-section h-screen block flex-shrink-0 absolute lg:static left-0 top-0 z-1 border-right-1 surface-border select-none">
-                      <div>
-                        <header className="header-style">
-                          <Button type="button" ref={closeIconRef as React.Ref<Button>} onClick={(e) => hide(e)} className="button-menu"></Button>
-                        </header>
-                        <div>
-                          <a href="/proctoring-results" className="menu-item" >
-                            <div className="menu-item-text">Результаты</div>
-                          </a>
-                          <a href="/proctoring-types" className="menu-item" >
-                            <div className="menu-item-text">Типы прокторинга</div>
-                          </a>
-                          <a href="/proctoring" className="menu-item" >
-                            <div className="menu-item-text">Прокторинги</div>
-                          </a>
-                          {username === 'admin' && (
-                            <>
-                              <a href="/roles" className="menu-item">
-                                <div className="menu-item-text">Роли</div>
-                              </a>
-                              <a href="/users" className="menu-item">
-                                <div className="menu-item-text">Пользователи</div>
-                              </a>
-                            </>
-                          )}
-                          <a href="/subjects" className="menu-item">
-                            <div className="menu-item-text">Предметы</div>
-                          </a>
-                        </div>
-                      </div>
-                      <div>
-                        <footer className="footer-style" />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              ></Sidebar>
+              <SidebarMenu
+                visible={visible}
+                onHide={() => setVisible(false)}
+                username={username}
+              />
             </div>
             <div className="user-exit">
               <span className="username">{username}</span>
